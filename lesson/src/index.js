866a76ae342450153aa16aaf7963e33a72c186a1
Se1fer <yyp673270171@sina.com>
2019-10-05 08:50:23 +0800

    loader学习&plugin学习
    loader学习&plugin学习

--- lesson/src/index.js
+++ lesson/src/index.js
@@ -10,7 +10,7 @@
 import Sidebar from './sidebar.js';
 import Content from './content.js'; */
 import avatar from './1.jpg';
-import './index.scss';
+import style from './index.scss';
 
 
 var Header = require('./header.js');
@@ -23,7 +23,7 @@ new Content();
 
 var img = new Image();
 img.src = avatar;
-img.classList.add('avatar');
+img.classList.add(style.avatar);
 
 var root = document.getElementById('root');
 root.append(img);
--- lesson/webpack.config.js
+++ lesson/webpack.config.js
@@ -1,4 +1,8 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin') /* htmlWebpackPlugin 会在打包结束后，自动生成一个html文件，兵吧打包生成的js自动引入到这个html文件中 */
+const { CleanWebpackPlugin } = require('clean-webpack-plugin') /* 3.0以上 cleanWebpackPlugin需要进行结构使用，这是个大坑 */
+    /* plugin  可以在webpack运行到某个时刻的时候，帮你做一些事情*/
+
 module.exports = {
     mode: 'production' /* webpack默认会配置production */ ,
     entry: {
@@ -23,12 +27,31 @@ module.exports = {
             },
             {
                 test: /\.scss$/,
-                use: ['style-loader', 'css-loader', 'sass-loader'] /* 执行顺序是从下往上，从右往左 */
+                use: [
+                    'style-loader',
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            importLoaders: 2, //可以保证在scss文件中引入其他scss，也可以保证正确使用
+                            modules: true
+                        }
+                    },
+                    'sass-loader',
+                    'postcss-loader'
+                ] /* 执行顺序是从下往上，从右往左 */
             }
         ]
     },
+
     output: {
-        filename: 'bundle.js',
+        publicPath: 'http://cdn.com.cn',
+        filename: '[name].js',
         path: path.resolve(__dirname, 'dist') /* 如果不写，默认也是这个目录 __dirname 就是当前目录，然后跟 dist进行拼接*/
-    }
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: 'src/index.html'
+        })
+    ]
 }
